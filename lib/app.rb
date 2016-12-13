96a2b4e597389cbf4604eaf2c68dbf7435df8fe4
Ariel Spear <arielspear@github.com>
2016-12-13 00:04:33 -0800

    time over

--- lib/app.rb
+++ lib/app.rb
@@ -4,5 +4,9 @@ class App
   def prompt
     puts 'How many questions do you want?';
   end
+  def getNumOfQ()
+    numOfQ = gets.chomp
+    return 'foo'
+  end
 end
 App.new.prompt
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,6 +7,7 @@ describe "App" do
     expect(STDOUT).to receive(:puts).with('How many questions do you want?')
     App.new.prompt
   end
+
   it 'should prompt for input and accept it' do
     allow($stdin).to receive(:gets).and_return('3')
     numOfQ = $stdin.gets.to_i
@@ -14,5 +15,6 @@ describe "App" do
     expect(numOfQ).to eq(3)
   end
 
-
-end
+  it 'should reject numbers not greater than 0'
+    App.new.getNumOfQ().should == "foo"
+  end
