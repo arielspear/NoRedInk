441acbd4294795b0cb354ef687d594d36157b5ff
Ariel Spear <arielspear@github.com>
2016-12-14 17:57:50 -0800

    returns question_id of a question

--- app.rb
+++ app.rb
@@ -5,10 +5,10 @@ class App
 
 
 
-  @@arr_of_arrs = CSV.read("./questions.csv")
+  @@arr_of_arrs = CSV.read("./questions.csv", :headers => true)
   def getNumOfQ()
     numOfQ = gets.chomp.to_i
-    puts numOfQ
+    # puts numOfQ
     if numOfQ < 1
         puts 'Please enter a number greater than 0'
       return numOfQ
@@ -16,19 +16,22 @@ class App
       return numOfQ
     end
   end
-  CSV.foreach('./questions.csv', :headers => true) do |csv_obj|
 
-    puts csv_obj['question_id'] #prints the question_id of each object(question)
+  # Array.new @arrayOfQ
+  # CSV.foreach('./questions.csv', :headers => true) do |csv_obj|
+  #
+  #   puts csv_obj['question_id'] #prints the question_id of each object(question)
+  #   @arrayOfQ.push(csv_obj)
+  # end
 
-
-  end
-
+  # puts @arrayOfQ
+  # IDK if I will need this object stuff later, but I'm leaving it here for now
 
     def prompt
 
       puts 'How many questions do you want?';
       num = getNumOfQ
-      puts @@arr_of_arrs[num]
+      puts @@arr_of_arrs[num]['question_id']
     end
 end
 App.new.prompt
