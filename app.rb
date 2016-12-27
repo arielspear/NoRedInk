d59552a35370e0c83f51b815b34c50699b9b92b3
Ariel Spear <arielspear@github.com>
2016-12-27 11:19:32 -0800

    Hash grouped by strand_id

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'stringio'
 require 'CSV'
+require 'pp'
 
 class App
 
@@ -26,6 +27,8 @@ class App
   # puts @arrayOfQ
   # IDK if I will need this object stuff later, but I'm leaving it here for now
 
+
+
     def prompt
       puts 'How many questions do you want?';
       num = getNumOfQ
@@ -35,6 +38,18 @@ class App
         puts @@arr_of_arrs[i]['question_id']
         i += 1
       end
+
+      strand_ids = {}
+
+      # make a hash with an empty array, then append the array with a Hash based on strand_ids
+      CSV.foreach("questions.csv", :headers => true, :header_converters => :symbol, :converters => :all) do |row|
+        strand_ids[row.fields[0]] = []
+      end
+      CSV.foreach("questions.csv", :headers => true, :header_converters => :symbol, :converters => :all) do |row|
+        strand_questions = strand_ids[row.fields[0]]
+        strand_questions.push Hash[row.headers[1..-1].zip(row.fields[1..-1])]
+      end
+      pp strand_ids
     end
 end
 App.new.prompt
